46626d3cf3ab7178a959a05dd7e6b97039ac0f80
natapaulis <natalia.sokalska@gmail.com>
2016-11-23 21:45:06 +0100

    Correct calculator functionality, change search field, improve repsonsivity

--- js/app.js
+++ js/app.js
@@ -1,45 +1,51 @@
 $(document).ready(function(){
-  var $loanValue = $("#value");
-  var $loanYears = $("#years");
-  var $paymentTotal = $("#total");
-  var $money = $loanValue.text();
-  var $months = $loanYears.text();
-  var $count = $money/$months * 1.02;
-  var $menuNav = $('.navbar li');
-  var $bars = $(".menu-bars i");
-  console.log($bars);
+    var $loanValue = $("#value");
+    var $loanYears = $("#years");
+    var $paymentTotal = $("#total");
+    var $money = 1000;
+    var $months = 1;
+    var $rate;
+    var $menuNav = $('.navbar li');
+    var $bars = $(".menu-bars i");
 
-  $('.sliderMoney').slider({
-    max: 60000,
-    min: 1000,
-    step: 100,
-    change: function( event, ui ) { }
-  });
+    $('.sliderMoney').slider({
+        max: 60000,
+        min: 1000,
+        step: 100,
+        change: function( event, ui ) { }
+    });
 
-  $('.sliderYears').slider({
-    max: 96,
-    min: 1,
-    change: function( event, ui ) { }
-  });
+    $('.sliderYears').slider({
+        max: 96,
+        min: 1,
+        change: function( event, ui ) { }
+    });
 
 
-function calculatorMoney(){
-  $( ".sliderMoney" ).on( "slidechange", function( event, ui ) {
-    $loanValue.text(ui.value);
-    $paymentTotal.text($count);
-  } );
-}
-function calculatorMonth(){
-  $( ".sliderYears" ).on( "slidechange", function( event, ui ) {
-    $loanYears.text(ui.value);
-    $paymentTotal.text($count);
-  } );
-  }
+    function calculatorMoney(){
+        $( ".sliderMoney" ).on( "slidechange", function( event, ui ) {
+            var $userMoney = ui.value;
+            $loanValue.text($userMoney);
+            $money = Number($loanValue.text());
+            $rate = Math.round($money/$months * 1.02);
+            $paymentTotal.text($rate);
+        });
+    }
+    function calculatorMonth(){
+        $( ".sliderYears" ).on( "slidechange", function( event, ui ) {
+            var $userTime = ui.value;
+            $loanYears.text($userTime);
+            $months = Number($loanYears.text());
+            $rate = Math.round($money/$months * 1.02);
+            $paymentTotal.text($rate);
+        });
+    }
 
-  calculatorMoney();
-  calculatorMonth();
+    calculatorMoney();
+    calculatorMonth();
 
-  $bars.on("click", function(){
-    $menuNav.toggleClass("active");
-  });
+    $bars.on("click", function(){
+        $menuNav.slideToggle()
+        $menuNav.toggleClass("active");
+    });
 });
